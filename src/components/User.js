fa19f8fb1f83abe85488594cecc25922c0d4e004
Dylan <dj0759@gmail.com>
2017-09-01 12:34:41 -0700

    first commit

---
+++ src/components/User.js
@@ -0,0 +1,123 @@
+import React from "react";
+import { Link } from "react-router";
+import GithubUser from '../components/GithubUser';
+
+const apiURL = "https://api.github.com/users/";
+
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 1,
+      loading: true,
+      user: []
+    };
+    this.fetchUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.params.username !== this.props.params.username) {
+      this.fetchUser();
+    }
+  }
+
+  //TODO: fetchUser class method needed meow
+  fetchUser() { 
+    console.log(this.props.params.username);
+    fetch(
+      `${apiURL}${this.props.params.username}`
+    )
+      .then(response => response.json())
+      .then(payload => {
+        if (payload.length === 0) {
+          this.setState({
+            loading:false,
+            done:true
+          });
+        } else {
+          this.setState({
+            user: payload,
+            loading: false
+          });
+      }});
+  }
+
+  /*
+    This method is used as a mapping function. Eventually this could be factored out to its own component.
+    */
+  renderStat(stat) {
+    return (
+      <li key={stat.name} className="user-info__stat">
+        <Link to={stat.url}>
+          <p className="user-info__stat-value">
+            {stat.value}
+          </p>
+          <p className="user-info__stat-name">
+            {stat.name}
+          </p>
+        </Link>
+      </li>
+    );
+  }
+
+  render() {
+    //if state has no user property render this, otherwise continue on
+    if (!this.state.user) {
+      return <div className="user-page">LOADING......</div>;
+    }
+
+    //assuming the user is loaded, bc we check
+    const user = this.state.user;
+
+    // Gather up some number stats about the user, to be used in a map below
+    const stats = [
+      {
+        name: "Public Repos",
+        value: user.public_repos,
+        url: `/user/${this.props.params.username}/repos`
+      },
+      {
+        name: "Followers",
+        value: user.followers,
+        url: `/user/${this.props.params.username}/followers`
+      },
+      {
+        name: "Following",
+        value: user.following,
+        url: `/user/${this.props.params.username}/following`
+      }
+    ];
+
+    //Now we can render, sweet
+    return (
+      <div className="user-page">
+        <div className="user-info">
+          <Link className="user-info__text" to={`/user/${user.login}`}>
+            <img
+              className="user-info__avatar"
+              src={user.avatar_url}
+              alt={`${user.login} avatar`}
+            />
+            <h2 className="user-info__title">
+              {user.login} ({user.name})
+            </h2>
+            <p className="user-info__bio">
+              {user.bio}
+            </p>
+          </Link>
+
+          <ul className="user-info__stats">
+            {stats.map(this.renderStat)}
+          </ul>
+        </div>
+        <div className="user-extra" />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default User;
